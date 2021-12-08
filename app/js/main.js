7418a265fe2a14aac36f650f0bf1ab6104969c87
O'Donoghue, Torsten <torsten.odonoghue@student.fhws.de>
2021-12-08 17:19:26 +0100

    JS

--- app/js/main.js
+++ app/js/main.js
@@ -1,31 +1,57 @@
 let startCapital = 2500
 let container = document.getElementById("player-container")
-let players = []
 
-players = {
-  "Torsten": startCapital,
-  "Reinhard": startCapital,
-  "Gudrun": startCapital,
-  "Gail": startCapital,
-  "Frank": startCapital,
-}
+let players = [
+  {
+    "name": "Torsten",
+    "capital": startCapital,
+    "lost": false
+  },
+  {
+    "name": "Reinhard",
+    "capital": startCapital,
+    "lost": false
+  },
+  {
+    "name": "Gudrun",
+    "capital": startCapital,
+    "lost": false
+  },
+  {
+    "name": "Frank",
+    "capital": startCapital,
+    "lost": false
+  },
+  {
+    "name": "Gail",
+    "capital": startCapital,
+    "lost": false
+  }
+]
 
-for (let player in players) {
-  addPlayer(container, player)
+updatePlayers()
+
+function updatePlayers() {
+  document.querySelectorAll('.player').forEach(e => e.remove());
+  for (let player of players) {
+    addPlayer(container, player)
+  }
 }
 
 function addPlayer(parent, player) {
 
+  let playerName = player["name"]
+  let playerCapital = player["capital"]
+
   let playerColor = getRandomColor()
   let element = `
     <div class="player" id="${player}">
-
       <div class="player-colorbox" style="background-color: var(${playerColor});"></div>
 
       <div class="player-info">
-        <span class="player-name" style="color: var(${playerColor});">${player}</span><br>
+        <span class="player-name" style="color: var(${playerColor});">${playerName}</span><br>
+        <span class="player-money">${playerCapital}</span>
         <span class="player-monopoly-money-icon">M</span>
-        <span class="player-money">${players[player]}</span>
       </div>
 
       <div class="player-seperator"></div>
@@ -41,7 +67,7 @@ function addPlayer(parent, player) {
         <span class="player-button-text">Add Salary</span>
       </div>
 
-      <div class="player-button player-button-accent" style="background-color: var(${playerColor});" onclick="">
+      <div class="player-button player-button-accent" style="background-color: var(${playerColor});" onclick="openTransactionPopup('${playerName}')">
         <div class="player-button-icon player-button-icon-accent">
           <svg width="44" height="44" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path
@@ -57,7 +83,7 @@ function addPlayer(parent, player) {
   parent.insertAdjacentHTML('beforeend', element);
 
   function getRandomColor() {
-    return "--color-player-accent-" + randomIntFromInterval(1, 8)
+    return "--color-player-accent-" + randomIntFromInterval(2, 2)
 
     function randomIntFromInterval(min, max) { // min and max included
       return Math.floor(Math.random() * (max - min + 1) + min)
@@ -67,54 +93,68 @@ function addPlayer(parent, player) {
 
 
 
+let mode = "subtract"
 
-function openTransactionPopup(parent, player) {
-  let element = `
-    <div class="popup-blur">
-      <div class="popup-container">
-        <div class="popup-close" onclick="this.parentElement.parentElement.remove()">
-          <svg width="44" height="44" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M4.39705 4.55379L4.46967 4.46967C4.73594 4.2034 5.1526 4.1792 5.44621 4.39705L5.53033 4.46967L12 10.939L18.4697 4.46967C18.7626 4.17678 19.2374 4.17678 19.5303 4.46967C19.8232 4.76256 19.8232 5.23744 19.5303 5.53033L13.061 12L19.5303 18.4697C19.7966 18.7359 19.8208 19.1526 19.6029 19.4462L19.5303 19.5303C19.2641 19.7966 18.8474 19.8208 18.5538 19.6029L18.4697 19.5303L12 13.061L5.53033 19.5303C5.23744 19.8232 4.76256 19.8232 4.46967 19.5303C4.17678 19.2374 4.17678 18.7626 4.46967 18.4697L10.939 12L4.46967 5.53033C4.2034 5.26406 4.1792 4.8474 4.39705 4.55379L4.46967 4.46967L4.39705 4.55379Z"
-              fill="currentColor" />
-          </svg>
-        </div>
+function openTransactionPopup(player) {
 
-        <span class="popup-balance-text">${player} Current Balance</span>
-        <div class="popup-balance-container">
-          <span class="player-monopoly-money-icon">M</span>
-          <span class="player-money">450</span>
-        </div>
-        <div class="popup-switch">
-          <div class="popup-switch-option popup-switch-option-selected">
-            Subtract
-          </div>
-          <div class="popup-switch-option">
-            Add
-          </div>
-        </div>
+  let popup = document.getElementById("id-popup-transaction")
+  let name = document.getElementById("id-popup-balance-text")
+  let switchSubtract = document.getElementById("id-popup-switch-subtract")
+  let switchAdd = document.getElementById("id-popup-switch-add")
+  let currentBalance = document.getElementById("id-popup-balance")
+  let buttonAccept = document.getElementById("id-popup-button-accept")
+  let textbox = document.getElementById("id-popup-textbox")
+  let modeSymbol = document.getElementById("popup-mode")
+  let audio = new Audio('app/audio/cash.mp3');
 
-        <div class="popup-textbox-container">
-          <span style="font-size: 64px; color: var(--color-player-text-2); padding: 8px;">-</span>
-          <input type="number" class="popup-textbox" placeholder="0" min="0" max="100000">
-        </div>
+  let index = players.findIndex((element, index) => {
+    if (element.name === player) {
+      return true
+    }
+  })
 
-        <div class="popup-button">
-          <svg width="44" height="44" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2ZM12 3.5C7.30558 3.5 3.5 7.30558 3.5 12C3.5 16.6944 7.30558 20.5 12 20.5C16.6944 20.5 20.5 16.6944 20.5 12C20.5 7.30558 16.6944 3.5 12 3.5ZM10.75 13.4393L15.2197 8.96967C15.5126 8.67678 15.9874 8.67678 16.2803 8.96967C16.5466 9.23594 16.5708 9.6526 16.3529 9.94621L16.2803 10.0303L11.2803 15.0303C11.0141 15.2966 10.5974 15.3208 10.3038 15.1029L10.2197 15.0303L7.71967 12.5303C7.42678 12.2374 7.42678 11.7626 7.71967 11.4697C7.98594 11.2034 8.4026 11.1792 8.69621 11.3971L8.78033 11.4697L10.75 13.4393L15.2197 8.96967L10.75 13.4393Z"
-              fill="currentColor" />
-          </svg>
-          <span style="padding-left: 8px; vertical-align: middle;">
-            Accept
-          </span>
+  currentBalance.innerHTML = players[index]["capital"]
 
-        </div>
+  switchSubtract.onclick = () => {
+    switchAdd.classList.remove("popup-switch-option-selected")
+    switchSubtract.classList.add("popup-switch-option-selected")
+    mode = "subtract"
+    modeSymbol.innerHTML = "-"
+    modeSymbol.style.color = "var(--color-player-accent-5)"
+  };
 
-      </div>
-    </div>
-  `
-  parent.insertAdjacentHTML('beforeend', element);
+  switchAdd.onclick = () => {
+    switchAdd.classList.add("popup-switch-option-selected")
+    switchSubtract.classList.remove("popup-switch-option-selected")
+    mode = "add"
+    modeSymbol.innerHTML = "+"
+    modeSymbol.style.color = "var(--color-player-accent-7)"
+  };
 
+  buttonAccept.onclick = () => {
+    console.log(index);
+    switch (mode) {
+      case "subtract": {
+        players[index]["capital"] -= parseInt(document.getElementById("id-popup-textbox").value)
+      } break
+      case "add": {
+        players[index]["capital"] += parseInt(document.getElementById("id-popup-textbox").value)
+      } break
+    }
+    audio.play();
+    updatePlayers()
+    closeTransactionPopup()
+  };
+
+  name.innerHTML = `${player}\'s Current Balance`
+  popup.style.display = "flex"
+
+}
+
+function closeTransactionPopup() {
+
+  let popup = document.getElementById("id-popup-transaction")
+
+  popup.style.display = "none"
 
 }
