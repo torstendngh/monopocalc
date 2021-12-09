80f613581ae82659e815c385ce9cf0d0218013e6
O'Donoghue, Torsten <torsten.odonoghue@student.fhws.de>
2021-12-09 21:49:58 +0100

    Added animations

--- app/js/main.js
+++ app/js/main.js
@@ -12,26 +12,36 @@ let players = [
   {
     name: "Player 1",
     capital: startCapital,
+    token: "hat",
+    capitalchange: false,
     lost: false,
   },
   {
     name: "Player 2",
     capital: startCapital,
+    token: "barrow",
+    capitalchange: false,
     lost: false,
   },
   {
     name: "Player 3",
     capital: startCapital,
+    token: "ship",
+    capitalchange: false,
     lost: false,
   },
   {
     name: "Player 4",
     capital: startCapital,
+    token: "thimble",
+    capitalchange: false,
     lost: false,
   },
   {
     name: "Player 5",
     capital: startCapital,
+    token: "dog",
+    capitalchange: false,
     lost: false,
   },
 ];
@@ -51,21 +61,39 @@ function updatePlayers() {
 function addPlayer(parent, player) {
   let playerName = player["lost"] ? "👻 " + player["name"] : player["name"];
   let playerCapital = player["capital"];
+  let playerToken = player["token"];
   let playerAlive = player["lost"] ? "player-dead" : "";
+  let playerWiggle = player["capitalchange"] ? "jello-horizontal" : "";
+  let playerAddMoney = player["capitalchange"] ? "money-add" : "";
   let inRed =
     playerCapital < 0
       ? "var(--color-player-accent-5)"
       : "var(--color-player-text-1)";
 
+  player["capitalchange"] = false
   let playerColor = getRandomColor();
   let element = `
     <div class="player ${playerAlive}" id="${player}">
       <div class="player-colorbox" style="background-color: var(${playerColor});"></div>
 
+      <div class="${playerWiggle}" style="
+        background-size: contain;
+        background-repeat: no-repeat;
+        background-position: center;
+        background-image: url('/app/images/pieces/${playerToken}.png');
+        align-self: center;
+        height: 120px;
+        width: 200px;
+        margin-left: 16px;
+      "></div>
+
       <div class="player-info">
         <span class="player-name" style="color: var(${playerColor});">${playerName}</span><br>
-        <span class="player-money" style="color: ${inRed}">${playerCapital}</span>
-        <span class="player-monopoly-money-icon">M</span>
+        <div class="${playerAddMoney}" style="display: inline-block;">
+          <span class="player-money" style="color: ${inRed}">${playerCapital}</span>
+          <span class="player-monopoly-money-icon">M</span>
+        </div>
+
       </div>
 
       <div class="player-seperator"></div>
@@ -73,11 +101,10 @@ function addPlayer(parent, player) {
       <div class="player-button" onclick="giveUp('${playerName}')">
         <div class="player-button-icon">
           <svg width="44" height="44" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M7.75 12C7.75 12.9665 6.9665 13.75 6 13.75C5.0335 13.75 4.25 12.9665 4.25 12C4.25 11.0335 5.0335 10.25 6 10.25C6.9665 10.25 7.75 11.0335 7.75 12Z" fill="currentColor"/>
-<path d="M13.75 12C13.75 12.9665 12.9665 13.75 12 13.75C11.0335 13.75 10.25 12.9665 10.25 12C10.25 11.0335 11.0335 10.25 12 10.25C12.9665 10.25 13.75 11.0335 13.75 12Z" fill="currentColor"/>
-<path d="M18 13.75C18.9665 13.75 19.75 12.9665 19.75 12C19.75 11.0335 18.9665 10.25 18 10.25C17.0335 10.25 16.25 11.0335 16.25 12C16.25 12.9665 17.0335 13.75 18 13.75Z" fill="currentColor"/>
-</svg>
-
+            <path d="M7.75 12C7.75 12.9665 6.9665 13.75 6 13.75C5.0335 13.75 4.25 12.9665 4.25 12C4.25 11.0335 5.0335 10.25 6 10.25C6.9665 10.25 7.75 11.0335 7.75 12Z" fill="currentColor"/>
+            <path d="M13.75 12C13.75 12.9665 12.9665 13.75 12 13.75C11.0335 13.75 10.25 12.9665 10.25 12C10.25 11.0335 11.0335 10.25 12 10.25C12.9665 10.25 13.75 11.0335 13.75 12Z" fill="currentColor"/>
+            <path d="M18 13.75C18.9665 13.75 19.75 12.9665 19.75 12C19.75 11.0335 18.9665 10.25 18 10.25C17.0335 10.25 16.25 11.0335 16.25 12C16.25 12.9665 17.0335 13.75 18 13.75Z" fill="currentColor"/>
+          </svg>
         </div>
         <span class="player-button-text">More</span>
       </div>
@@ -95,12 +122,10 @@ function addPlayer(parent, player) {
         <span class="player-button-text">Passed Go</span>
       </div>
 
-      <div class="player-button player-button-accent" style="background-color: var(${playerColor});" onclick="openTransactionPopup('${playerName}')">
+      <div class="player-button player-button-accent" style="border: none; background-color: var(${playerColor});" onclick="openTransactionPopup('${playerName}')">
         <div class="player-button-icon player-button-icon-accent">
           <svg width="44" height="44" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M16 8.25C16 7.83579 16.3358 7.5 16.75 7.5C17.1642 7.5 17.5 7.83579 17.5 8.25V11.5C17.5 11.9142 17.1642 12.25 16.75 12.25H14C13.5858 12.25 13.25 11.9142 13.25 11.5C13.25 11.0858 13.5858 10.75 14 10.75H15.2705C14.7674 9.43442 13.4924 8.5 12.0001 8.5C10.907 8.5 9.96324 8.96361 9.32682 9.72938C9.06206 10.0479 8.5892 10.0916 8.27064 9.8268C7.95208 9.56205 7.90846 9.08918 8.17321 8.77062C9.09595 7.66036 10.4632 7 12.0001 7C13.636 7 15.0879 7.78531 16 8.99955V8.25ZM8 15V15.75C8 16.1642 7.66421 16.5 7.25 16.5C6.83579 16.5 6.5 16.1642 6.5 15.75V12.75C6.5 12.3358 6.83579 12 7.25 12H10C10.4142 12 10.75 12.3358 10.75 12.75C10.75 13.1642 10.4142 13.5 10 13.5H8.83669C9.39859 14.6829 10.6047 15.5 12.0001 15.5C13.0707 15.5 14.0368 15.0204 14.6786 14.2655C14.9469 13.9499 15.4202 13.9115 15.7358 14.1798C16.0514 14.4481 16.0897 14.9214 15.8214 15.237C14.9041 16.316 13.5252 17 12.0001 17C10.3638 17 8.91194 16.2143 8 15ZM12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM20.5 12C20.5 16.6944 16.6944 20.5 12 20.5C7.30558 20.5 3.5 16.6944 3.5 12C3.5 7.30558 7.30558 3.5 12 3.5C16.6944 3.5 20.5 7.30558 20.5 12Z"
-              fill="currentColor" />
+            <path d="M16 8.25C16 7.83579 16.3358 7.5 16.75 7.5C17.1642 7.5 17.5 7.83579 17.5 8.25V11.5C17.5 11.9142 17.1642 12.25 16.75 12.25H14C13.5858 12.25 13.25 11.9142 13.25 11.5C13.25 11.0858 13.5858 10.75 14 10.75H15.2705C14.7674 9.43442 13.4924 8.5 12.0001 8.5C10.907 8.5 9.96324 8.96361 9.32682 9.72938C9.06206 10.0479 8.5892 10.0916 8.27064 9.8268C7.95208 9.56205 7.90846 9.08918 8.17321 8.77062C9.09595 7.66036 10.4632 7 12.0001 7C13.636 7 15.0879 7.78531 16 8.99955V8.25ZM8 15V15.75C8 16.1642 7.66421 16.5 7.25 16.5C6.83579 16.5 6.5 16.1642 6.5 15.75V12.75C6.5 12.3358 6.83579 12 7.25 12H10C10.4142 12 10.75 12.3358 10.75 12.75C10.75 13.1642 10.4142 13.5 10 13.5H8.83669C9.39859 14.6829 10.6047 15.5 12.0001 15.5C13.0707 15.5 14.0368 15.0204 14.6786 14.2655C14.9469 13.9499 15.4202 13.9115 15.7358 14.1798C16.0514 14.4481 16.0897 14.9214 15.8214 15.237C14.9041 16.316 13.5252 17 12.0001 17C10.3638 17 8.91194 16.2143 8 15ZM12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM20.5 12C20.5 16.6944 16.6944 20.5 12 20.5C7.30558 20.5 3.5 16.6944 3.5 12C3.5 7.30558 7.30558 3.5 12 3.5C16.6944 3.5 20.5 7.30558 20.5 12Z" fill="currentColor" />
           </svg>
         </div>
         <span class="player-button-text player-button-text-accent">Transaction</span>
@@ -127,6 +152,7 @@ function addSalary(player) {
     }
   });
   players[index]["capital"] += salary;
+  players[index]["capitalchange"] = true;
   updatePlayers();
   playSound(audioCash);
 }
@@ -186,6 +212,7 @@ function openTransactionPopup(player) {
           }
           break;
       }
+      players[index]["capitalchange"] = true;
       playSound(audioCash);
       updatePlayers();
       closeTransactionPopup(false);
